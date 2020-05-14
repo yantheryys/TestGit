46830f3d3497e796f9839a1d50bc9fcb83660182
yanther_glb <yanther@163.com>
2020-05-14 09:12:39 +0800

    process error

--- src/main/java/com/wz/git/Apple.java
+++ src/main/java/com/wz/git/Apple.java
@@ -3,7 +3,7 @@ package com.wz.git;
 public class Apple {
 	public static void main(String[] args) {
 		System.out.println("测试git的原程序 right");
-		System.out.println("测试git的原程序 left")
+		System.out.println("测试git的原程序 left");
 	}
 
 }
